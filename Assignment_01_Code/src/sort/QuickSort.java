41b076913bb680fad19e8a78bb6d495b74a650cf
João David <joaodavid512@gmail.com>
2019-10-14 22:35:36 +0100

    added seq version of quicksort using a recursive approach

--- Assignment_01_Code/src/sort/QuickSort.java
+++ Assignment_01_Code/src/sort/QuickSort.java
@@ -2,6 +2,7 @@ package sort;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.RecursiveTask;
 
 public class QuickSort<E extends Comparable<E>> extends RecursiveTask<List<E>> {
@@ -13,18 +14,29 @@ public class QuickSort<E extends Comparable<E>> extends RecursiveTask<List<E>> {
 
 	public static void main(String[] args) {
 		
-
+		Random gen = new Random();
 		List<Integer> list = new ArrayList<Integer>();
-		list.add(2);
+		for(int i = 0; i < 5000; i++) {
+			list.add(gen.nextInt(5000)-5000);
+		}
+		/*list.add(2);
 		list.add(3);
 		list.add(1);
 		list.add(12);
 		list.add(5);
 		list.add(0);
+		list.add(-10);*/
 		
-
-		QuickSort<Integer> f = new QuickSort(list);
-		System.out.println("Sorted = " + f.compute());
+		
+		QuickSort<Integer> f = new QuickSort<Integer>(list);
+		long tPar = System.nanoTime();
+		System.out.println("Sorted Par. = " + f.compute());
+		System.out.println("parallelComputation (sec): " + ((System.nanoTime() - tPar)));
+		
+		long tSeq = System.nanoTime();
+		System.out.println("Sorted Seq. = " + quicksort(list));
+		System.out.println("sequentialComputation (sec): " + ((System.nanoTime() - tSeq)));
+		
 	}
 
 	
@@ -40,6 +52,9 @@ public class QuickSort<E extends Comparable<E>> extends RecursiveTask<List<E>> {
 		if (array.size() <= 1) {
 			return array;
 		}
+		/*if (array.size() <= 1000) {
+			return quicksort(array);
+	    }*/
 		E pivot = array.remove(0);
 		List<E> less = new ArrayList<E>();
 		List<E> higher = new ArrayList<E>();
@@ -51,8 +66,8 @@ public class QuickSort<E extends Comparable<E>> extends RecursiveTask<List<E>> {
 			}
 		}
 	
-		QuickSort<E> qs1 = new QuickSort(less);
-		QuickSort<E> qs2 = new QuickSort(higher);
+		QuickSort<E> qs1 = new QuickSort<E>(less);
+		QuickSort<E> qs2 = new QuickSort<E>(higher);
 		
 		qs1.fork();
 		qs2.fork();
@@ -62,6 +77,27 @@ public class QuickSort<E extends Comparable<E>> extends RecursiveTask<List<E>> {
 		newList.addAll((List<E>) qs2.join());
 		return newList;
 		
+	}
+	
+	public static <E extends Comparable<E>> List<E> quicksort(List<E> list) {
+		if (list.size() <= 1) {
+			return list;
+		}
+		E pivot = list.remove(0);
+		List<E> less = new ArrayList<E>();
+		List<E> higher = new ArrayList<E>();
+		for (E i : list) {
+			if (i.compareTo(pivot) <= 0) {
+				less.add(i);
+			}else {
+				higher.add(i);
+			}
+		}
+	
+		List<E> newList = new ArrayList<E>(quicksort(less));
+		newList.add(pivot);
+		newList.addAll(quicksort(higher));
+		return newList;
 	}
 	
 	
