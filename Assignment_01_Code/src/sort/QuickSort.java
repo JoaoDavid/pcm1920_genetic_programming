9bae59ff4e44cc3a0f61c4e9396643a4ecebd754
João David <joaodavid512@gmail.com>
2019-10-14 19:01:59 +0100

    finished quicksort algorithm using List<Integer>

--- Assignment_01_Code/src/sort/QuickSort.java
+++ Assignment_01_Code/src/sort/QuickSort.java
@@ -1,38 +1,67 @@
 package sort;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-public class QuickSort extends RecursiveTask {
+public class QuickSort extends RecursiveTask<List<Integer>> {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		
+
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(2);
+		list.add(3);
+		list.add(1);
+		list.add(12);
+		list.add(5);
+		list.add(0);
+		
+
+		QuickSort f = new QuickSort(list);
+		System.out.println("Sorted = " + f.compute());
+	}
 
 	
-	private int[] array;
-	private int pivot;
-	private boolean hasPivot;
+	private List<Integer> array;
 	
-	public QuickSort(int[] array) {
+	public QuickSort(List<Integer> array) {
 		this.array = array;
-		hasPivot = false;
 	}
-	
-	private QuickSort(int[] array, int pivot) {
-		this.array = array;
-		this.pivot = pivot;
-		hasPivot = true;
-	}
+
 
 	@Override
-	protected int[] compute() {
-		if (array.length == 1) {
+	protected List<Integer> compute() {
+		if (array.size() <= 1) {
 			return array;
 		}
-		int middle = array.length / 2;
-		Stream.concat(Arrays.stream(array), Arrays.stream(array))
-        .toArray(int[]::new);
-		QuickSort qs1 = new QuickSort(Arrays.copyOfRange(array, 0, middle), array[0]);
-		QuickSort qs2 = new QuickSort(Arrays.copyOfRange(array, middle + 1, array.length - 1), array[0]);
+		int pivot = array.remove(0);
+		List<Integer> less = new ArrayList<Integer>();
+		List<Integer> higher = new ArrayList<Integer>();
+		for (Integer i : array) {
+			if (i <= pivot) {
+				less.add(i);
+			}else {
+				higher.add(i);
+			}
+		}
+	
+		QuickSort qs1 = new QuickSort(less);
+		QuickSort qs2 = new QuickSort(higher);
+		
+		qs1.fork();
+		qs2.fork();
+
+		List<Integer> newList = new ArrayList<Integer>((List<Integer>) qs1.join());
+		newList.add(pivot);
+		newList.addAll((List<Integer>) qs2.join());
+		return newList;
+		
 	}
 	
 	
