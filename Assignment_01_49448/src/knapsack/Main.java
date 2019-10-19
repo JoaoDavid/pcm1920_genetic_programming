bcac0d36bfdbc1f420e3b1231c9b73dd189c65eb
unknown <joaodavid512@gmail.com>
2019-10-19 16:34:46 +0100

    finished converting seq version of KnapsackGA

--- Assignment_01_49448/src/knapsack/Main.java
+++ Assignment_01_49448/src/knapsack/Main.java
@@ -3,6 +3,8 @@ package knapsack;
 public class Main {
 	public static void main(String[] args) {
 		KnapsackGA ga = new KnapsackGA();
+		long time = System.nanoTime();
 		ga.run();
+		System.out.println(System.nanoTime() - time + "nano sec");
 	}
 }
