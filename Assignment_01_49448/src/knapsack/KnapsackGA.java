147d5efa2684e75035cbcf5c85840877478c1a96
João Pedro Vieira David <joaodavid512@gmail.com>
2019-10-31 19:53:24 +0000

    improved knapsack

--- Assignment_01_49448/src/knapsack/KnapsackGA.java
+++ Assignment_01_49448/src/knapsack/KnapsackGA.java
@@ -9,16 +9,17 @@ public class KnapsackGA {
 	private static final int N_GENERATIONS = 500;
 	private static final int POP_SIZE = 100000;
 	private static final double PROB_MUTATION = 0.5;	
-	private Individual[] population;
-	
+	private Individual[] population = new Individual[POP_SIZE];
+
 	public KnapsackGA() {
 		populateInitialPopulationRandomly();
 	}
 
 	private void populateInitialPopulationRandomly() {
-		population = IntStream.range(0, POP_SIZE).parallel().mapToObj( num -> {
-			return Individual.createRandom();
-		}).toArray(Individual[]::new);
+		IntStream.range(0, POP_SIZE).parallel().forEach(i -> {
+			population[i] = Individual.createRandom();
+		});
+
 	}
 
 	public void run() {
@@ -26,41 +27,35 @@ public class KnapsackGA {
 
 			// Step1 - Calculate Fitness
 			Arrays.stream(population).parallel().forEach(Individual::measureFitness);
-			
-			
+
 			// Step2 - Sort by Fitness descending			
-			population = Arrays.stream(population).parallel().sorted(new Comparator<Individual>() {
+			Arrays.parallelSort(population, new Comparator<Individual>() {
 				@Override
 				public int compare(Individual o1, Individual o2) {
 					if (o1.getFitness() > o2.getFitness()) return -1;
 					if (o1.getFitness() < o2.getFitness()) return 1;
 					return 0;
-				}
-			}).toArray(Individual[]::new);
-			
-						
+				}});
+
 			// Debug			
 			Individual best = population[0];
 			System.out.println("Best fitness at " + generation + " is " + best.getFitness());
-
 			
-			// Step3 - Find parents to mate (cross-over)			
-			Stream<Individual> newStreamPop = IntStream.range(1, POP_SIZE).parallel().mapToObj( num -> {
+			Individual[] newPopulation = new Individual[POP_SIZE];
+			newPopulation[0] = best;
+			IntStream.range(1, POP_SIZE).parallel().forEach(i -> {				
+				// Step3 - Find parents to mate (cross-over)
 				// The first elements in the population have higher probability of being selected
 				int pos1 = (int) (- Math.log(ThreadLocalRandom.current().nextDouble()) * POP_SIZE) % POP_SIZE;
 				int pos2 = (int) (- Math.log(ThreadLocalRandom.current().nextDouble()) * POP_SIZE) % POP_SIZE;
-				return population[pos1].crossoverWith(population[pos2]);
+				newPopulation[i] = population[pos1].crossoverWith(population[pos2]);
+				
+				// Step4 - Mutate
+				if (ThreadLocalRandom.current().nextDouble() < PROB_MUTATION) {
+					newPopulation[i].mutate();
+				}
 			});
-			
-			
-			// Step4 - Mutate			
-			Individual[] newPopulation = newStreamPop.toArray(Individual[]::new);
-			Stream.of(newPopulation).parallel().forEach(i -> {
-		        if (ThreadLocalRandom.current().nextDouble() < PROB_MUTATION) {
-		            i.mutate();
-		        }
-		    });
-			
+
 			population = Stream.concat(Stream.of(best),Stream.of(newPopulation)).toArray(Individual[]::new);
 		}
 	}
